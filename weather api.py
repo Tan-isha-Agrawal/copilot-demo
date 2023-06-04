ae5056884290be5fe16176cdd32a4f694d30da56
Tan-isha-Agrawal <73835297+Tan-isha-Agrawal@users.noreply.github.com>
2023-06-04 19:01:17 +0530

    Update weather api.py

--- weather api.py
+++ weather api.py
@@ -5,62 +5,28 @@ import requests
 import json
 import sys
 
-# api-endpoint
-# URL = "http://api.openweathermap.org/data/2.5/weather?"
-
-# # location given here
-# location = sys.argv[1]
-
-# # defining a params dict for the parameters to be sent to the API
-# PARAMS = {'q':location, 'appid':'b1b15e88fa797225412429c1c50c122a1'}
-
-# # sending get request and saving the response as response object
-# r = requests.get(url = URL, params = PARAMS)
-
-# # extracting data in json format
-# data = r.json()
-
-# # extracting temperature, humidity, pressure and description
-# temp = data['main']['temp']
-# humidity = data['main']['humidity']
-# pressure = data['main']['pressure']
-# description = data['weather'][0]['description']
-
-# # printing the output
-# print("Temperature (in kelvin unit) = " +   str(temp) + 
-#         "\nHumidity (in percentage) = " + str(humidity) +
-#         "\nAtmospheric pressure (in hPa unit) = " + str(pressure) +
-#         "\nDescription = " + str(description))
 
 #convert above logic to function
 def weather(location):
     URL = "http://api.openweathermap.org/data/2.5/weather?"
-    PARAMS = {'q':location, 'appid':'b1b15e88fa797225412429c1c50c122a1'}
+    PARAMS = {'q':location, 'appid':'3921a6d2246b41b25bfc5a5a7458f9a7'}
     r = requests.get(url = URL, params = PARAMS)
     data = r.json()
     try:
-        data['main']['temp']
+        print(data['main']['temp'])
     except:
         print("Please enter a valid city name")
         return
     if data['cod'] == 401:
-        print("Please enter a valid city name")
-        return
+        print("enter a valid api key")
     if data['cod'] == 404:
-        print("Please enter a valid city name")
-        return
-    if data['cod'] == 429:
-        print("Please enter a valid city name")
+        print("enter a valid city name")
         return
     if data['cod'] == 500:
-        print("Please enter a valid city name")
+        print("internal server error")
         return
-    if data['cod'] == 503:
-        print("Please enter a valid city name")
-        return
-    if data['cod'] == 504:
-        print("Please enter a valid city name")
-        return
+
+       
     if data['cod'] == 200:
         
         temp = data['main']['temp']
@@ -71,10 +37,10 @@ def weather(location):
             "\nHumidity (in percentage) = " + str(humidity) +
             "\nAtmospheric pressure (in hPa unit) = " + str(pressure) +
             "\nDescription = " + str(description))
-location = sys.argv[1]
+# location = sys.argv[1]
 location=input("Enter the city name: ")
-try:
-    weather(location)
-except:
 
-    print("Please enter a valid city name")
+weather(location)
+
+#sample input: python weather api.py london
+#sample input: python weather api.py london,uk
